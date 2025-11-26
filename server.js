d071650df73e1afbcb4f3dab0f7423c021464264
emn5805 <muhammettirnakci5@gmail.com>
2025-11-26 13:51:58 +0300

    commit v3

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server is running on https://butcetakip.onrender.com`);
 });
